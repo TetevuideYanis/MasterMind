c478fe4abfd10736acb27a73e017b69a9116b4d5
TetevuideYanis <tetevuide.yanis@gmail.com>
2023-09-13 09:07:49 +0200

    finalisamtion de la fonction qui compare les couleurs

--- mastermind.py
+++ mastermind.py
@@ -7,9 +7,24 @@ et de couleurs partiellement correctes
 def compare(dico1, dico2):
     correct = 0
     partial = 0
+    dico1Colors = {"R" : 0, "V" : 0, "B" : 0, "J" : 0, "M" : 0, "N" : 0}
+    dico2Colors = {"R" : 0, "V" : 0, "B" : 0, "J" : 0, "M" : 0, "N" : 0}
     for i in range(4):
+        #compte le nombre de couleur
+        dico1Colors[dico1[i]] += 1
+        dico2Colors[dico2[i]] += 1
+        #si les couleurs sont les mêmes au même emplacement alors on ajoute une couleur correcte
         if(dico1[i] == dico2[i]):
             correct += 1
+    for i in range(4):
+        #si une couleur est présente dans les 2 dico alors on l'ajoute dans les partiels
+        if(dico1Colors[dico1[i]] > 0 and dico2Colors[dico1[i]] > 0):
+            partial += 1
+            #on la retire pour éviter les faux doublons 
+            dico2Colors[dico1[i]] -= 1
+    partial -= correct
+    return {"correct" : correct, "partial" : partial}
+                
             
 
 colors = ["R", "V", "B", "J", "M", "N"]
@@ -18,6 +33,7 @@ code = {0 : "", 1 : "", 2 : "", 3 : ""}
 #tire le code au hasard parmi les couleurs de la liste
 for i in range(4):
     code[i] = random.choice(colors)
+print(code)
 
 guessStr = ""
 guessDico = {0 : "", 1 : "", 2 : "", 3 : ""}
@@ -29,5 +45,7 @@ while(guessDico != code and tries <= 12):
     #transforme le string en dico
     for i in range(4):
         guessDico[i] = guessStr[i]
+    hint = compare(code, guessDico)
+    print("Correct : " + str(hint["correct"]) + " | Partiel : " + str(hint["partial"]))
     if(guessDico == code):
-        print("Trouvé en " + str(12 - tries))
+        print("Trouvé en " + str(tries))
